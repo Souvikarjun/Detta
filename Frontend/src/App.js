a8f02649ccaf8b9363b41f52f83e800c4595f258
Souvikarjun <68196981+Souvikarjun@users.noreply.github.com>
2024-07-28 11:59:02 +0530

    setting up login page

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route, /*useNavigate*/ } from 'react-router-dom';
-import Login from "./components/components";
+import Login from "./components/Login";
 import Home from "./container/Home";
 
 
